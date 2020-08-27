2af531f003c0dadba24f5a1f8409666e058277b3
Ross Netterville <ross.nettervillekurtosys.com@SAMAC091.local>
2020-08-27 16:34:58 +0200

    initial committ

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const passDoors = (doors, passNumber) => {
+    let data = Array.from(doors);
+    for(let i = passNumber - 1; i< data.length; i+=passNumber){
+        if (data[i] === '#'){
+            data[i] = '@';
+        } else if (data[i] === '@'){
+            data[i] = '#';
+        }
+    }
+    //convert back to string
+    const output = data.toString().replace(/,/gi, '');
+    printDoors(output);
+    //return it
+    return output;   
+} 
+
+const printDoors = (doors) =>{
+    console.log(doors);
+}
+
+module.exports = {
+    passDoors: passDoors,
+    printDoors: printDoors
+}
+
+//To Do: add the printDoors function
+
+//output = passDoors(output)
